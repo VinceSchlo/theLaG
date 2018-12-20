b2aaf59e30029c6433684978e7527510e40e87f7
Vincent Schlosser <s.vincent@live.fr>
2018-12-20 11:00:42 +0100

    Search availabilities

--- app/controllers/BookingController.php
+++ app/controllers/BookingController.php
@@ -2,6 +2,7 @@
 
 require_once('vendor/thelag/AppController.php');
 require_once('app/models/User.php');
+require_once('app/models/Availability.php');
 
 class BookingController extends AppController
 {
@@ -21,11 +22,24 @@ class BookingController extends AppController
                 break;
 
             case 'availabilities': 
+
+
+                if (isset($_GET['year']) && isset($_GET['month']) && isset($_GET['day'])) {
+
+                    $date = $_GET['year'] . '-' . $_GET['month'] . '-' . $_GET['day'];
+
+                    $availabilitie = new Availabilitie();
+                    $allAvailabilities = $availabilitie->getAvailabilitiesByDate($date);
+
+                }
+
+
                 echo $this->twig->render('availabilities/availabilities.html.twig', [
                     'nowDate' => getdate(),
                     'days' => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31],
                     'months' => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-                    'years' => [2018, 2019, 2020]
+                    'years' => [2018, 2019, 2020],
+                    'allAvailabilities' => $allAvailabilities,
                 ]);
 
                 break;
