3b6b83f974a0959fc3de5cdf0ad84e90c1492a36
Félix Darraud <felix.darraud@gmail.com>
2018-12-20 14:25:25 +0100

    WIP on booking

--- app/controllers/BookingController.php
+++ app/controllers/BookingController.php
@@ -33,7 +33,30 @@ class BookingController extends AppController
 
                 echo $this->twig->render('booking/booking.html.twig', [
                     'availability' => $availability,
-                    'coach' => $availability->user
+                    'coach' => $availability->user,
+                    'days' => [
+                        '0' => 'Lundi',
+                        '1' => 'Mardi',
+                        '2' => 'Mercredi',
+                        '3' => 'Jeudi',
+                        '4' => 'Vendredi',
+                        '5' => 'Samedi',
+                        '6' => 'Dimanche'
+                    ],
+                    'months' => [
+                        '1' => 'Janvier',
+                        '2' => 'Février',
+                        '3' => 'Mars',
+                        '4' => 'Avril',
+                        '5' => 'Mai',
+                        '6' => 'Juin',
+                        '7' => 'Juillet',
+                        '8' => 'Aout',
+                        '9' => 'Septembre',
+                        '10' => 'Octobre',
+                        '11' => 'Novembre',
+                        '12' => 'Décembre'
+                    ]
                 ]);
 
                 break;
